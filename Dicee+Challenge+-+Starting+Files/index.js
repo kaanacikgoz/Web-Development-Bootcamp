361c99687057bad9e63233c66e2eb00875453b6d
Kaan Acikgoz <kaanacikgoz@outlook.com>
2025-01-10 20:19:11 +0300

    Add Dicee Challenge

--- Dicee+Challenge+-+Starting+Files/index.js
+++ Dicee+Challenge+-+Starting+Files/index.js
@@ -1,5 +1,5 @@
-var randomNumber1 = Math.floor(Math.random(1)*6)+1
-var randomNumber2 = Math.floor(Math.random(1)*6)+1
+var randomNumber1 = Math.floor(Math.random()*6)+1
+var randomNumber2 = Math.floor(Math.random()*6)+1
 
 document.querySelector(".img1").setAttribute("src", "./images/dice"+randomNumber1+".png")
 
